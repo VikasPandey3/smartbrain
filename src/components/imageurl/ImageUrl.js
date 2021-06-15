d6730d679dca963d5586632cf1cf02cc75124b85
VikasPandey3 <52690920+VikasPandey3@users.noreply.github.com>
2021-06-15 20:45:05 +0530

    signin and signup page added

--- src/components/imageurl/ImageUrl.js
+++ src/components/imageurl/ImageUrl.js
@@ -5,7 +5,7 @@ const ImageUrl = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div className="center">
       <div className="center form">
-        <input type="text" onChange={onInputChange} />
+        <input type="text" onChange={onInputChange} placeholder="Enter image url with .jpg,.png " />
         <button onClick={onButtonSubmit}>Detect</button>
       </div>
     </div>
